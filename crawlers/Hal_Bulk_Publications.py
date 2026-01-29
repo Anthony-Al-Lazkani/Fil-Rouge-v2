7bb087c32a0d7b870ca52837ad1780fa111e9adb
Guillaume DAVID <off.guillaume.david@gmail.com>
2026-01-29 16:51:05 +0100

    adaptation du crawler HAL, multiple queries

--- crawlers/Hal_Bulk_Publications.py
+++ crawlers/Hal_Bulk_Publications.py
@@ -1,7 +1,19 @@
 """
+USAGE:
+    uv run crawlers/Hal_Bulk_Publications.py 
+    => va générer un fichier 'hal_publications.jsonl' dans /data/
+
+QUERY: 'intelligence artificielle' + "artificial intelligence" from 2019
+
+EXPLICATIONS
+Fonctionne en complément avec Hal_fetcher_publications.py
 Logique métier + écriture fichier
 Il n'y a pas de données country renseignées sur HAL - l'exemple exposé ci-dessous doit être une exception
 voir: https://api.archives-ouvertes.fr/search/?q=intelligence%20artificielle&rows=1&fl=*&wt=json pour obtenir les noms des champs
+
+
+
+
 """
 
 import json
@@ -9,43 +21,51 @@ from Hal_fetcher_publications import HALPublicationFetcher
 from pathlib import Path
 
 
-QUERY = '"intelligence artificielle"'
+QUERIES = [
+    '"intelligence artificielle"',
+    '"artificial intelligence"',
+]
 START_YEAR = 2019
 MAX_RECORDS = 10_000
 
 Path("data").mkdir(parents=True, exist_ok=True)
 
 fetcher = HALPublicationFetcher()
-start = 0
-count = 0
 
 
-with open("data/hal_publications.jsonl", "w", encoding="utf-8") as f:
-    while count < MAX_RECORDS:
-        docs = fetcher.fetch(QUERY, START_YEAR, start=start)
-        if not docs:
-            break
-
-        for doc in docs:
-            record = {
-                "publication": {
-                    "id": doc.get("halId_s"),
-                    "title": doc.get("title_s"),
-                    "year": doc.get("producedDateY_i"),
-                    "doi": doc.get("doiId_s"),
-                    "domains": doc.get("domain_s", []),
-                    "keywords": doc.get("keyword_s", []),
-                },
-
-                "authors": doc.get("authFullName_s", [])
-            }
-            f.write(json.dumps(record, ensure_ascii=False) + "\n")
-            count += 1
-
-            if count >= MAX_RECORDS:
+with open("data/hal_publications.jsonl", "a", encoding="utf-8") as f:
+    
+    for query in QUERIES:
+        start = 0
+        count = 0
+    
+        while count < MAX_RECORDS:
+            docs = fetcher.fetch(query, START_YEAR, start=start)
+            if not docs:
                 break
 
-        start += fetcher.rows
-        print(f"{count} notices collectées…", end="\r")
+            for doc in docs:
+                record = {
+                    "source":"HAL",
+                    "query":query,
+                    "publication": {
+                        "id": doc.get("halId_s"),
+                        "title": doc.get("title_s"),
+                        "year": doc.get("producedDateY_i"),
+                        "doi": doc.get("doiId_s"),
+                        "domains": doc.get("domain_s", []),
+                        "keywords": doc.get("keyword_s", []),
+                    },
 
+                    "authors": doc.get("authFullName_s", [])
+                }
+                f.write(json.dumps(record, ensure_ascii=False) + "\n")
+                count += 1
+
+                if count >= MAX_RECORDS:
+                    break
+
+            start += fetcher.rows
+            print(f"{count} notices collectées…", end="\r")
+    print(f"\nRequête terminée : {query}")
 print("\nTéléchargement terminé.")
